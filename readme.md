c990f894ccd04709e5283d16931eba7e74d81112
Marc Russwurm <marc.russwurm@epfl.ch>
2022-03-10 13:46:29 +0100

    added setup.py and other minors

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Bag-of-MAML
 
+![](https://github.com/marccoru/bagofmaml/actions/workflows/python-package-conda.yml/badge.svg)
+
 ```commandline
 pip install bagofmaml
 ```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as req_file:
+    requirements = [req.strip() for req in req_file.read().splitlines()]
+
+setup(
+    name='bagofmaml',
+    version='0.1.0',
+    author="Marc Rußwurm",
+    author_email="marc.russwurm@epfl.ch",
+    packages=find_packages(include=['bagofmaml', 'bagofmaml.*']),
+    install_requires=requirements,
+)
